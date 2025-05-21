959e68ba220d70bf890e43b224609a8c166cbac9
daffnr <daffanr20@gmail.com>
2025-05-22 01:09:54 +0700

    deploy

--- Components/Screens/CategoryPanel.js
+++ Components/Screens/CategoryPanel.js
@@ -35,7 +35,6 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:"center",
-        // backgroundColor:'#fff',
         width:'100%'
     },
     myButton:{
